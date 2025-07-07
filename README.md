2b8bb71623a869551715450d180d18c6c2515825
Noble Eugene <nobleeugene2005@gmail.com>
2025-07-07 08:44:50 +0100

    fix readme

--- README.md
+++ README.md
@@ -41,10 +41,15 @@ yinctl --help
 https://github.com/user-attachments/assets/e97912b1-6d31-4ee8-800b-73f1c1de1a7e
 
 
-
 # TODO
 - Resize to best resolution for monitor
 - Use DMABUF instead of wl_shm
 - Maybe some slide transitions with pixman
 - Cleanup god awful code
+
+
+# IMPORTANT THINGS TO NOTE
+- While this can play MP4s, it does so by extracting, compressing and storing the frames and then later loading it all into memory. It is optimised for videos and gifs that do not change alot in between frames so really most things you'll find on live wallpaper websites. It should use significantly less cpu but if you want to play highres very long videos, other tools like mpvpaper will probably serve you better.
+
+- I built this mainly to solve a problem i was having with swww(converting videos to gif which causes a lot banding and quality losses) and to learn how to use the Zig Programming Language. I also only have one laptop screen so if you try to use this with a multi monitor setup, expect catastrophic failure. If you still go ahead and encounter any issues which you will, please file a specific issue and i'll see what i can do about it. Contributions are also welcome.
 
---
+++ yin_daemon/image_REMOTE_284646.zig
@@ -0,0 +1,118 @@
+const std = @import("std");
+const Buffer = @import("Buffer.zig");
+const pixman = @import("pixman");
+const wl = @import("wayland").client.wl;
+const lz4 = @import("shared").lz4;
+const Output = @import("output.zig").Output;
+const zigimg = @import("zigimg");
+const allocator = @import("util.zig").allocator;
+const animation = @import("animation.zig");
+pub const Image = @This();
+src: *pixman.Image,
+pixel_data: std.ArrayList(u32),
+
+pub const ImageResponse = union(enum) {
+    Static: struct {
+        image: *Image,
+    },
+    Animated: struct {
+        image: animation.AnimatedImage,
+    },
+};
+pub fn load_image(path: []const u8, output: *Output) !?ImageResponse {
+    const _file = try std.fs.openFileAbsolute(path, .{});
+    var file = try allocator.create(std.fs.File);
+    file.* = _file;
+    //determine whether it is animated or static
+    const static_or_animated_len = try file.reader().readInt(u8, .little);
+    var _buffer = try allocator.alloc(u8, static_or_animated_len);
+    defer allocator.free(_buffer);
+    const _br = try file.reader().readAll(_buffer);
+
+    if (std.mem.order(u8, _buffer[0.._br], "animated") == .eq) {
+        return animation.load(file, output);
+    } else if (std.mem.order(u8, _buffer[0.._br], "static") != .eq) {
+        //unknown, possibly corrupted.
+        return null;
+    }
+    defer allocator.destroy(file);
+    defer file.close();
+    //read original
+    const original_len = try file.reader().readInt(u32, .little);
+    //read compressed len
+    const compressed_len = try file.reader().readInt(u32, .little);
+    //read height
+    const height = try file.reader().readInt(u32, .little);
+    //read width
+    const width = try file.reader().readInt(u32, .little);
+    //read stride
+    const stride = try file.reader().readInt(u8, .little);
+    //read data
+    const bytes_to_read = compressed_len;
+    const compressed_data_buffer = try allocator.alloc(u8, bytes_to_read);
+    defer allocator.free(compressed_data_buffer);
+
+    _ = try file.reader().readAll(compressed_data_buffer);
+    const original_size: u32 = original_len * @sizeOf(u32);
+    const decompressed_buffer = try allocator.alloc(u8, original_size);
+    defer allocator.free(decompressed_buffer);
+    const decompressed_size = lz4.LZ4_decompress_safe(
+        @ptrCast(@alignCast(compressed_data_buffer.ptr)),
+        @ptrCast(@alignCast(decompressed_buffer.ptr)),
+        @intCast(compressed_data_buffer.len),
+        @intCast(decompressed_buffer.len),
+    );
+    const decompressed_data_slice = decompressed_buffer[0..@intCast(decompressed_size)];
+    const decompressed_data = std.mem.bytesAsSlice(u32, decompressed_data_slice);
+    var pixel_data = std.ArrayList(u32).init(allocator);
+    try pixel_data.resize(decompressed_data.len);
+    @memcpy(pixel_data.items, decompressed_data);
+    const src_img = pixman.Image.createBits(.a8r8g8b8, @intCast(width), @intCast(height), @as([*]u32, @ptrCast(@alignCast(pixel_data.items.ptr))), @intCast(stride * width)) orelse return error.NoPixmanImage;
+    const src = try allocator.create(Image);
+    src.* = .{ .src = src_img, .pixel_data = pixel_data };
+    return ImageResponse{ .Static = .{ .image = src } };
+}
+
+
+pub fn deinit(image: *Image) void {
+    image.pixel_data.deinit(); //destroy pixel data
+    _ = image.src.unref();
+    allocator.destroy(image);
+}
+
+fn calculate_scale(image_dimension: c_int, output_dimension: u32, scale: u32) f64 {
+    const numerator: f64 = @floatFromInt(image_dimension);
+    const denominator: f64 = @floatFromInt(output_dimension * scale);
+
+    return numerator / denominator;
+}
+
+/// Calculates (image_dimension / dimension_scale - output_dimension) / 2 / dimension_scale;
+fn calculate_transform(image_dimension: c_int, output_dimension: u32, dimension_scale: f64) f64 {
+    const numerator1: f64 = @floatFromInt(image_dimension);
+    const denominator1: f64 = dimension_scale;
+    const subtruend: f64 = @floatFromInt(output_dimension);
+    const numerator2: f64 = numerator1 / denominator1 - subtruend;
+
+    return numerator2 / 2 / dimension_scale;
+}
+//transform, thanks beanbag
+pub fn Scale(self: *Image, width: u32, height: u32, scale: u32) void {
+    var image = self.src;
+    var sx: f64 = @as(f64, @floatFromInt(image.getWidth())) / @as(f64, @floatFromInt(width * scale));
+    var sy: f64 = calculate_scale(image.getHeight(), height, scale);
+    const s = if (sx > sy) sy else sx;
+    sx = s;
+    sy = s;
+    const tx: f64 = calculate_transform(image.getWidth(), width, sx);
+    const ty: f64 = calculate_transform(image.getWidth(), height, sy);
+
+    var t: pixman.FTransform = undefined;
+    var t2: pixman.Transform = undefined;
+
+    pixman.FTransform.initTranslate(&t, tx, ty);
+    pixman.FTransform.initScale(&t, sx, sy);
+    _ = pixman.Transform.fromFTransform(&t2, &t);
+    _ = image.setTransform(&t2);
+    _ = image.setFilter(.best, &[_]pixman.Fixed{}, 0);
+}
