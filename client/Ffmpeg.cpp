9321399ba399ba5f71240ab9be6598c160772233
Saverin <eugeneforce@proton.me>
2026-07-02 06:29:48 +0100

    small ffmpeg improvment

--- client/Ffmpeg.cpp
+++ client/Ffmpeg.cpp
@@ -3,20 +3,21 @@
 #include <stdexcept>
 #include <string_view>
 
-void cacheVideo(std::string_view file_path, std::string_view write_to, u32 width, u32 height) {
+void cacheVideo(std::string_view file_path, std::string_view write_to,
+                u32 width, u32 height) {
   std::filesystem::path full_path = file_path;
 
   std::ostringstream command;
-command << "ffmpeg "
-        << "-y "
-        << "-i \"" << full_path.string() << "\" "
-        << "-vf scale=" << width << ":" << height << " "
-        << "-pix_fmt yuv420p "
-        << "-profile:v main "
-        << "-level:v 4.1 "
-        << "-an "
-        << "-c:v libx264 "
-        << "\"" << write_to << "\"";
+  command << "ffmpeg "
+          << "-y "
+          << "-i \"" << full_path.string() << "\" "
+          << "-vf scale=" << width << ":" << height << " "
+          << "-pix_fmt yuv420p "
+          << "-profile:v main "
+          << "-level:v 4.1 "
+          << "-an "
+          << "-c:v libx264 "
+          << "\"" << write_to << "\"";
   int result = std::system(command.str().c_str());
   if (result != 0) {
     throw std::runtime_error("ffmpeg failed");
